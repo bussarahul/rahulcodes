f4c1025c586f7dbf67693b0cc2672ebdb33c2701
bussarahul <103060752+bussarahul@users.noreply.github.com>
2022-04-14 01:26:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ $("a[href^='#']").click(function(e) {
 		scrollTop: position
 	} /* speed */ );
 });
+
+$(".navbar-toggler").click(function(e) {
+	e.preventDefault();
+	$(this).toggleClass('collapsed');
+	var ariaEx = JSON.parse($(this).attr('aria-expanded'));
+	$(this).attr('aria-expanded', !ariaEx); 
+	var datatarget = $(this).attr('data-target');
+	console.log(datatarget);
+	$(datatarget).toggleClass('show');
+})
